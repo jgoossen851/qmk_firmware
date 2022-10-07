b6159c23e8e59c7641d8c05c336be3c8b10529d8
Jeremy Goossen <jeremyg995@gmail.com>
2022-10-07 12:41:02 -0500

    Switch GUI and Pointer layer mods

--- keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
@@ -73,9 +73,10 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define _CUT    LCTL(KC_X)
 #define _UNDO   LCTL(KC_Z)
 
-#define _______________DEAD_HALF_ROW_______________  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
-#define ______________HOME_ROW_GACS_L______________  KC_LGUI, KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX
-#define ______________HOME_ROW_GACS_R______________  XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT, KC_LGUI
+#define ___X________X__DEAD_HALF_ROW__X________X___  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define ___X_______DEAD_KEYS_IV_______X___           XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX
+#define          ______HOME_ROW_ACS_L______________           KC_LALT, KC_LCTL, KC_LSFT, XXXXXXX
+#define _______________HOME_ROW_ACS_R_____           XXXXXXX, KC_LSFT, KC_LCTL, KC_LALT
 #define ________________CLIPBOARD_L________________    _UNDO,    _CUT,   _COPY,  _PASTE,   _REDO
 #define ________________CLIPBOARD_R________________    _REDO,  _PASTE,   _COPY,    _CUT,   _UNDO
 
@@ -98,19 +99,19 @@ static uint16_t auto_pointer_layer_timer = 0;
  * base layer to avoid having to layer change mid edit and to enable auto-repeat.
  */
 #define LAYOUT_LAYER_NAVIGATION                                                                \
-    _______________DEAD_HALF_ROW_______________,   KC_INS, KC_HOME,   KC_UP,  KC_END, KC_PGUP, \
-    ______________HOME_ROW_GACS_L______________,  KC_CLCK, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, \
-    _______________DEAD_HALF_ROW_______________,  ________________CLIPBOARD_R________________, \
+    ___X________X__DEAD_HALF_ROW__X________X___,   KC_INS, KC_HOME,   KC_UP,  KC_END, KC_PGUP, \
+    XXXXXXX, ______HOME_ROW_ACS_L______________,  KC_CLCK, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, \
+    KC_LGUI, ___X_______DEAD_KEYS_IV_______X___,  ________________CLIPBOARD_R________________, \
                       XXXXXXX, _______, XXXXXXX,   KC_ENT, KC_BSPC
 
 /** \brief Mouse emulation and pointer functions. 
  *
  * Secondary right-hand layer
  */
-#define LAYOUT_LAYER_POINTER                                                                  \
+#define LAYOUT_LAYER_POINTER                                                                   \
     DPI_MOD, XXXXXXX, DRGSCRL, SNIPING, S_D_MOD,  XXXXXXX, KC_WH_L, DRGSCRL, KC_WH_R, KC_WH_U, \
-    ______________HOME_ROW_GACS_L______________,  XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_WH_D, \
-    XXXXXXX, LK_BASE,  LK_PTR, XXXXXXX, XXXXXXX,  ________________CLIPBOARD_R________________, \
+    XXXXXXX, ______HOME_ROW_ACS_L______________,  XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_WH_D, \
+    KC_LGUI, LK_BASE,  LK_PTR, XXXXXXX, XXXXXXX,  ________________CLIPBOARD_R________________, \
                       XXXXXXX, XXXXXXX, _______,  XXXXXXX,  KC_DEL
 
 /**
@@ -120,11 +121,11 @@ static uint16_t auto_pointer_layer_timer = 0;
  * symmetrical to accomodate the left- and right-hand trackball.
 
  */
-#define LAYOUT_LAYER_MEDIA                                                                    \
-    XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, \
-    KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, \
-    XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST,   QK_BOOT,   QK_BOOT, EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX, \
-                      _______, KC_MPLY, KC_MSTP, KC_MSTP, KC_MPLY
+#define LAYOUT_LAYER_MEDIA                                                                     \
+    XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX,  XXXXXXX,RGB_RMOD, RGB_TOG, RGB_MOD, XXXXXXX, \
+    KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT,  KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, \
+    XXXXXXX, XXXXXXX, XXXXXXX, EEP_RST, QK_BOOT,  QK_BOOT, EEP_RST, XXXXXXX, XXXXXXX, XXXXXXX, \
+                      _______, KC_MPLY, KC_MSTP,  KC_MSTP, KC_MPLY
 
 
 /**
@@ -134,11 +135,11 @@ static uint16_t auto_pointer_layer_timer = 0;
  * are in the standard numpad locations with symbols in the remaining positions.
  * `KC_DOT` is duplicated from the base layer.
  */
-#define LAYOUT_LAYER_NUMERAL                                                                  \
-    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC, _______________DEAD_HALF_ROW_______________, \
-    KC_SLSH,    KC_4,    KC_5,    KC_6,  KC_EQL, ______________HOME_ROW_GACS_R______________, \
-     KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS, _______________DEAD_HALF_ROW_______________, \
-                       KC_DOT,    KC_0, KC_MINS, XXXXXXX, _______
+#define LAYOUT_LAYER_NUMERAL                                                                   \
+    KC_LBRC,    KC_7,    KC_8,    KC_9, KC_RBRC,  ___X________X__DEAD_HALF_ROW__X________X___, \
+    KC_SLSH,    KC_4,    KC_5,    KC_6,  KC_EQL,  _______________HOME_ROW_ACS_R_____, XXXXXXX, \
+     KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,  ___X_______DEAD_KEYS_IV_______X___, KC_LGUI, \
+                       KC_DOT,    KC_0, KC_MINS,  XXXXXXX, _______
 
 /**
  * \brief Symbols layer.
@@ -147,11 +148,11 @@ static uint16_t auto_pointer_layer_timer = 0;
  * chording when using mods with shifted symbols. `KC_LPRN` is duplicated next to
  * `KC_RPRN`.
  */
-#define LAYOUT_LAYER_SYMBOLS                                                                  \
-    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR, _______________DEAD_HALF_ROW_______________, \
-    KC_QUES,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS, ______________HOME_ROW_GACS_R______________, \
-    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE, _______________DEAD_HALF_ROW_______________, \
-                      KC_LPRN, KC_RPRN, KC_UNDS, _______, XXXXXXX
+#define LAYOUT_LAYER_SYMBOLS                                                                   \
+    KC_LCBR, KC_AMPR, KC_ASTR, KC_LPRN, KC_RCBR,  ___X________X__DEAD_HALF_ROW__X________X___, \
+    KC_QUES,  KC_DLR, KC_PERC, KC_CIRC, KC_PLUS,  _______________HOME_ROW_ACS_R_____, XXXXXXX, \
+    KC_TILD, KC_EXLM,   KC_AT, KC_HASH, KC_PIPE,  ___X_______DEAD_KEYS_IV_______X___, KC_LGUI, \
+                      KC_LPRN, KC_RPRN, KC_UNDS,  _______, XXXXXXX
 
 /**
  * \brief Mouse emulation and pointer functions.
@@ -160,7 +161,7 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 #define LAYOUT_LAYER_POINTER2                                                                  \
     DPI_MOD, XXXXXXX, DRGSCRL, SNIPING, S_D_MOD,  XXXXXXX, KC_WH_L, DRGSCRL, KC_WH_R, KC_WH_U, \
-    ______________HOME_ROW_GACS_L______________,  XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_WH_D, \
+    XXXXXXX, ______HOME_ROW_ACS_L______________,  XXXXXXX, KC_BTN1, KC_BTN2, KC_BTN3, KC_WH_D, \
     ________________CLIPBOARD_L________________,  ________________CLIPBOARD_R________________, \
                       XXXXXXX, KC_BTN1, KC_BTN2,  XXXXXXX, XXXXXXX
 
@@ -173,10 +174,10 @@ static uint16_t auto_pointer_layer_timer = 0;
  * column. App is on the tertiary thumb key and other thumb keys are duplicated
  * from the base layer to enable auto-repeat.
  */
-#define LAYOUT_LAYER_FUNCTION                                                                 \
-    _______________DEAD_HALF_ROW_______________,  KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
-    ______________HOME_ROW_GACS_L______________,  KC_SLCK,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
-    _______________DEAD_HALF_ROW_______________,  KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
+#define LAYOUT_LAYER_FUNCTION                                                                  \
+    ___X________X__DEAD_HALF_ROW__X________X___,  KC_PSCR,   KC_F7,   KC_F8,   KC_F9,  KC_F12, \
+    XXXXXXX, ______HOME_ROW_ACS_L______________,  KC_SLCK,   KC_F4,   KC_F5,   KC_F6,  KC_F11, \
+    KC_LGUI, ___X_______DEAD_KEYS_IV_______X___,  KC_PAUS,   KC_F1,   KC_F2,   KC_F3,  KC_F10, \
                       XXXXXXX, XXXXXXX, _______,  XXXXXXX, XXXXXXX
 
 /**
@@ -192,11 +193,14 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define _HOME_ROW_MOD_GACS(                                            \
     L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
     L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
+    L20, L21, L22, L23, L24, R25, R26, R27, R28, R29,                  \
     ...)                                                               \
              L00,         L01,         L02,         L03,         L04,  \
              R05,         R06,         R07,         R08,         R09,  \
-      LGUI_T(L10), LALT_T(L11), LCTL_T(L12), LSFT_T(L13),        L14,  \
-             R15,  RSFT_T(R16), RCTL_T(R17), LALT_T(R18), RGUI_T(R19), \
+             L10,  LALT_T(L11), LCTL_T(L12), LSFT_T(L13),        L14,  \
+             R15,  RSFT_T(R16), RCTL_T(R17), LALT_T(R18),        R19, \
+      LGUI_T(L20),        L21,         L22,         L23,         L24,  \
+             R25,         R26,         R27,         R28,  RGUI_T(R29), \
       __VA_ARGS__
 #define HOME_ROW_MOD_GACS(...) _HOME_ROW_MOD_GACS(__VA_ARGS__)
 
@@ -210,17 +214,14 @@ static uint16_t auto_pointer_layer_timer = 0;
  *
  *     BUTTON_MOD(LAYER_ALPHAS_QWERTY)
  */
-#define _BUTTON_MOD(                                                  \
+#define _BUTTON_MOD(                                                   \
     L00, L01, L02, L03, L04, R05, R06, R07, R08, R09,                  \
     L10, L11, L12, L13, L14, R15, R16, R17, R18, R19,                  \
-    L20, L21, L22, L23, L24, R25, R26, R27, R28, R29,                  \
     ...)                                                               \
              L00,         L01,         L02,         L03,         L04,  \
              R05,         R06,         R07,         R08,         R09,  \
-             L10,         L11,         L12,         L13,         L14,  \
-             R15,         R16,         R17,         R18,         R19,  \
-      _L_BTN(L20),        L21,         L22,         L23,         L24,  \
-             R25,         R26,         R27,         R28,  _L_BTN(R29), \
+      _L_BTN(L10),        L11,         L12,         L13,         L14,  \
+             R15,         R16,         R17,         R18,  _L_BTN(R19), \
       __VA_ARGS__
 #define BUTTON_MOD(...) _BUTTON_MOD(__VA_ARGS__)
 
