3c4e18c8295d539adaba03993fa66812d204d1e4
Jeremy Goossen <jeremyg995@gmail.com>
2022-10-07 11:44:48 -0500

    Disable RGB matrix in keymap config

--- keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/rules.mk
+++ keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/rules.mk
@@ -1,1 +1,6 @@
 VIA_ENABLE = no
+
+RGB_MATRIX_SUPPORTED = no  # RGB matrix is supported and enabled by default
+RGBLIGHT_SUPPORTED = no    # RGB underglow is supported, but not enabled by default
+RGB_MATRIX_ENABLE = no     # Enable keyboard RGB matrix functionality
+RGB_MATRIX_DRIVER = WS2812
