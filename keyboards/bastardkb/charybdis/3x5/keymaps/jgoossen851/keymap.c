3a2fb25d96f4e090f5ec05f85216c5885a3b6c9f
Jeremy Goossen <jeremyg995@gmail.com>
2022-11-12 14:38:49 -0600

    Fix several keymap issues

--- keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
@@ -118,7 +118,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_SYMBOLS                                                                   \
      KC_ESC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,   KC_DEL, KC_SLSH, KC_QUES, XXXXXXX, KC_BSPC, \
      KC_TAB,  KC_DLR, KC_PERC, KC_CIRC, KC_MINS,  CAPSWRD, ___HOME_ROW_MODS_ACS_R___,  KC_ENT, \
-    KC_LBRC, KC_EXLM,   KC_AT, KC_HASH, KC_RBRC,  KC_CAPS, TO_SYMB, TO_NAVG, OSL_FUN, XXXXXXX, \
+    KC_LBRC, KC_EXLM,   KC_AT, KC_HASH, KC_RBRC,  KC_CAPS, TO_SYMB, TO_NAVG, OSL_FUN, KC_RGUI, \
                       _______, TO_ALPH, OSL_PNT,  TO_NUMB, TO_SYMB
 
 
@@ -131,7 +131,7 @@ static uint16_t auto_pointer_layer_timer = 0;
  */
 #define LAYOUT_LAYER_NUMERAL                                                                   \
      KC_ESC,    KC_7,    KC_8,    KC_9,  KC_EQL,   KC_DEL, KC_SLSH,    KC_0, KC_PDOT, KC_BSPC, \
-     KC_TAB,    KC_4,    KC_5,    KC_6, KC_MINS,  KC_COLN, OSM_SFT, KC_RCTL, KC_LALT, KC_PENT, \
+     KC_TAB,    KC_4,    KC_5,    KC_6, KC_MINS,  KC_COLN, OSM_SFT, KC_RCTL, KC_LALT,  KC_ENT, \
      KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,  XXXXXXX, TO_NUMB, TO_PNTR, TO_HXNP, KC_RGUI, \
                       _______, TO_ALPH, OSL_PNT,  TO_NUMB, OSL_SYM
 
@@ -175,7 +175,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_FUNCTION                                                                  \
      KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR,  XXXXXXX, XXXXXXX,  KC_APP, XXXXXXX, XXXXXXX, \
      KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SLCK,  XXXXXXX, ___HOME_ROW_MODS_ACS_R___, XXXXXXX, \
-     KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,  XXXXXXX, TO_FUNC, TO_TOOL, XXXXXXX, XXXXXXX, \
+     KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,  XXXXXXX, TO_FUNC, TO_TOOL, XXXXXXX, KC_RGUI, \
                       _______, TO_ALPH, OSL_PNT,  TO_NUMB, TO_SYMB
 
 
@@ -186,7 +186,7 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_MEDIA                                                                     \
     ___X________X__DEAD_HALF_ROW__X________X___,  XXXXXXX, KC_MSTP, KC_MPLY, XXXXXXX, XXXXXXX, \
     ___X________X__DEAD_HALF_ROW__X________X___,  KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, \
-    XXXXXXX, XXXXXXX, TO_HXNP, TO_MEDA, QK_BOOT,  XXXXXXX, XXXXXXX, TO_PNTR, XXXXXXX, TO_NUMB, \
+    XXXXXXX, XXXXXXX, TO_HXNP, TO_MEDA, XXXXXXX,  ___X________X__DEAD_HALF_ROW__X________X___, \
                       _______, TO_ALPH, OSL_PT2,  TO_NUMB, TO_SYMB
 
 
@@ -200,9 +200,9 @@ static uint16_t auto_pointer_layer_timer = 0;
  * work on this layer but Alt-codes can be used on Windows.
  */
 #define LAYOUT_LAYER_NUMPAD                                                                    \
-       KC_C,    KC_7,    KC_8,    KC_9,    KC_F,   KC_DEL, KC_SLSH,    KC_0, KC_PDOT, KC_BSPC, \
-       KC_B,    KC_4,    KC_5,    KC_6,    KC_E,  KC_COLN, XXXXXXX, XXXXXXX, KC_ASTR, KC_PENT, \
-       KC_A,    KC_1,    KC_2,    KC_3,    KC_D,  KC_CAPS, TO_HXNP, TO_MEDA, XXXXXXX, XXXXXXX, \
+       KC_C, KC_KP_7, KC_KP_8, KC_KP_9,    KC_F,   KC_DEL, KC_SLSH, KC_KP_0, KC_PDOT, KC_BSPC, \
+       KC_B, KC_KP_4, KC_KP_5, KC_KP_6,    KC_E,  KC_COLN, KC_ASTR, KC_MINS, KC_PLUS, KC_PENT, \
+       KC_A, KC_KP_1, KC_KP_2, KC_KP_3,    KC_D,  KC_CAPS, TO_HXNP, TO_MEDA, XXXXXXX, XXXXXXX, \
                       _______, TO_ALPH, OSL_PNT,  OSL_NUM, OSL_SYM
 
 /**
@@ -210,7 +210,7 @@ static uint16_t auto_pointer_layer_timer = 0;
  *
  */
 #define LAYOUT_LAYER_TOOL                                                                      \
-    ___X________X__DEAD_HALF_ROW__X________X___,  ___X________X__DEAD_HALF_ROW__X________X___, \
+    ___X________X__DEAD_HALF_ROW__X________X___,  ___X_______DEAD_KEYS_IV_______X___, QK_BOOT, \
     ___X________X__DEAD_HALF_ROW__X________X___,  ___X________X__DEAD_HALF_ROW__X________X___, \
     XXXXXXX, XXXXXXX, TO_FUNC, TO_TOOL, XXXXXXX,  ___X________X__DEAD_HALF_ROW__X________X___, \
                       _______, TO_ALPH, OSL_PT2,  TO_NUMB, TO_SYMB
