806071ea7d475424f8e018d12df726a4abd8814c
Jeremy Goossen <jeremyg995@gmail.com>
2022-11-16 12:37:54 -0600

    Fix layer one-shot ordering

--- keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
@@ -26,16 +26,17 @@ enum charybdis_keymap_layers {
     LAYER_NUMERAL,
     LAYER_SYMBOLS,
     LAYER_FUNCTION,
-    LAYER_POINTER_OSL,
+    LAYER_POINTER,
     LAYER_TOOL,
     LAYER_NAVIGATION,
     LAYER_MEDIA,
-    LAYER_POINTER_TO,
+    LAYER_POINTER_OSL,
+    LAYER_NAVIGATION_OSL,
 };
 
 // Automatically enable sniping-mode on the pointer layer.
 #define LAYER_MASK(layer) ((layer_state_t)1 << layer)
-#define CHARYBDIS_AUTO_SNIPING_ON_LAYER_MASK ( LAYER_MASK(LAYER_POINTER_OSL) | LAYER_MASK(LAYER_POINTER_TO) )
+#define CHARYBDIS_AUTO_SNIPING_ON_LAYER_MASK ( LAYER_MASK(LAYER_POINTER) | LAYER_MASK(LAYER_POINTER_OSL) )
 
 #ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
 static uint16_t auto_pointer_layer_timer = 0;
@@ -54,24 +55,24 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define OSL_SYM OSL(LAYER_SYMBOLS)
 #define OSL_NUM OSL(LAYER_NUMERAL)
 #define OSL_NAV OSL(LAYER_NAVIGATION)
-#define OSL_PNT OSL(LAYER_POINTER_OSL)
+#define OSL_PNT OSL(LAYER_POINTER)
 #define OSL_FUN OSL(LAYER_FUNCTION)
 #define OSL_MED OSL(LAYER_MEDIA)
 #define OSL_TOL OSL(LAYER_TOOL)
-#define OSL_PT2 OSL(LAYER_POINTER_TO)
+#define OSL_PT2 OSL(LAYER_POINTER_OSL)
+#define OSL_NV2 OSL(LAYER_NAVIGATION_OSL)
 #define AST_ALT MT(KC_ASTR, MOD_LALT)
 
 #define TO_ALPH TO(LAYER_BASE)
 #define TO_SYMB TO(LAYER_SYMBOLS)
 #define TO_NUMB TO(LAYER_NUMERAL)
 #define TO_NAVG TO(LAYER_NAVIGATION)
-#define TO_PNTR TO(LAYER_POINTER_OSL)
+#define TO_PNTR TO(LAYER_POINTER)
 #define TO_FUNC TO(LAYER_FUNCTION)
 #define TO_MEDA TO(LAYER_MEDIA)
 #define TO_HXNP TO(LAYER_NUMPAD)
 #define TO_TOOL TO(LAYER_TOOL)
 
-#define MO_PNTR MO(LAYER_POINTER_OSL)
 #define LNAV    LAYER_NAVIGATION
 #define LMED    LAYER_MEDIA
 #define LTOL    LAYER_TOOL
@@ -156,11 +157,11 @@ static uint16_t auto_pointer_layer_timer = 0;
  *
  * Secondary right-hand layer
  */
-#define LAYOUT_LAYER_POINTER(f)                                                                   \
+#define LAYOUT_LAYER_POINTER(f)                                                                \
      KC_ESC, DPI_MOD, DRGSCRL, SNP_TOG, KC_WH_U,   KC_DEL, KC_WH_L, DRG_TOG, KC_WH_R, KC_BSPC, \
      KC_TAB, ___HOME_ROW_MODS_ACS_L___, KC_WH_D,   KC_INS, KC_BTN1, KC_BTN2, KC_BTN3,  KC_ENT, \
     KC_LGUI, f(LTOL), TO_SYMB, TO_PNTR, S_D_MOD,  ________________CLIPBOARD_R________________, \
-                      TO_PNTR, TO_ALPH, f(LNAV),  TO_NUMB, TO_SYMB
+                      TO_PNTR, TO_ALPH, OSL_NV2,  TO_NUMB, TO_SYMB
 
 
 /**
@@ -187,8 +188,8 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_MEDIA                                                                     \
     ___X________X__DEAD_HALF_ROW__X________X___,  XXXXXXX, KC_MSTP, KC_MPLY, XXXXXXX, XXXXXXX, \
     ___X________X__DEAD_HALF_ROW__X________X___,  KC_MPRV, KC_VOLD, KC_MUTE, KC_VOLU, KC_MNXT, \
-    XXXXXXX, OSL_PT2, TO_HXNP, TO_MEDA, XXXXXXX,  ___X________X__DEAD_HALF_ROW__X________X___, \
-                      OSL_PT2, TO_ALPH, TO_NAVG,  TO_NUMB, TO_SYMB
+    XXXXXXX, TO_PNTR, TO_HXNP, TO_MEDA, XXXXXXX,  ___X________X__DEAD_HALF_ROW__X________X___, \
+                      OSL_PT2, TO_ALPH, OSL_NV2,  TO_NUMB, TO_SYMB
 
 
 /**
@@ -213,8 +214,8 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_LAYER_TOOL                                                                      \
     ___X________X__DEAD_HALF_ROW__X________X___,   EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, QK_BOOT, \
     ___X________X__DEAD_HALF_ROW__X________X___,  ___X________X__DEAD_HALF_ROW__X________X___, \
-    XXXXXXX, TO_NAVG, TO_FUNC, TO_TOOL, XXXXXXX,  ___X________X__DEAD_HALF_ROW__X________X___, \
-                      OSL_PT2, TO_ALPH, TO_NAVG,  TO_NUMB, TO_SYMB
+    XXXXXXX, OSL_NAV, TO_FUNC, TO_TOOL, XXXXXXX,  ___X________X__DEAD_HALF_ROW__X________X___, \
+                      OSL_PT2, TO_ALPH, OSL_NAV,  TO_NUMB, TO_SYMB
 
 
 /**
@@ -245,16 +246,17 @@ static uint16_t auto_pointer_layer_timer = 0;
 #define LAYOUT_wrapper(...) LAYOUT_charybdis_3x5(__VA_ARGS__)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [LAYER_BASE]       = LAYOUT_wrapper(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE)),
-  [LAYER_SYMBOLS]    = LAYOUT_wrapper(LAYOUT_LAYER_SYMBOLS),
-  [LAYER_NUMERAL]    = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
-  [LAYER_NAVIGATION] = LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
-  [LAYER_POINTER_OSL]    = LAYOUT_wrapper(LAYOUT_LAYER_POINTER(OSL)),
-  [LAYER_FUNCTION]   = LAYOUT_wrapper(LAYOUT_LAYER_FUNCTION),
-  [LAYER_MEDIA]      = LAYOUT_wrapper(LAYOUT_LAYER_MEDIA),
-  [LAYER_NUMPAD]     = LAYOUT_wrapper(LAYOUT_LAYER_NUMPAD),
-  [LAYER_TOOL]       = LAYOUT_wrapper(LAYOUT_LAYER_TOOL),
-  [LAYER_POINTER_TO]  = LAYOUT_wrapper(LAYOUT_LAYER_POINTER(TO)),
+  [LAYER_BASE]          = LAYOUT_wrapper(HOME_ROW_MOD_GACS(LAYOUT_LAYER_BASE)),
+  [LAYER_SYMBOLS]       = LAYOUT_wrapper(LAYOUT_LAYER_SYMBOLS),
+  [LAYER_NUMERAL]       = LAYOUT_wrapper(LAYOUT_LAYER_NUMERAL),
+  [LAYER_NAVIGATION]    = LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
+  [LAYER_POINTER]       = LAYOUT_wrapper(LAYOUT_LAYER_POINTER(OSL)),
+  [LAYER_FUNCTION]      = LAYOUT_wrapper(LAYOUT_LAYER_FUNCTION),
+  [LAYER_MEDIA]         = LAYOUT_wrapper(LAYOUT_LAYER_MEDIA),
+  [LAYER_NUMPAD]        = LAYOUT_wrapper(LAYOUT_LAYER_NUMPAD),
+  [LAYER_TOOL]          = LAYOUT_wrapper(LAYOUT_LAYER_TOOL),
+  [LAYER_POINTER_OSL]   = LAYOUT_wrapper(LAYOUT_LAYER_POINTER(TO)),
+  [LAYER_NAVIGATION_OSL]= LAYOUT_wrapper(LAYOUT_LAYER_NAVIGATION),
 };
 // clang-format on
 
@@ -293,6 +295,8 @@ layer_state_t layer_state_set_user(layer_state_t state) {
             LED_OFF(LED_4);
             break;
         case LAYER_NAVIGATION:
+        case LAYER_NAVIGATION_OSL:
+        case LAYER_POINTER:
         case LAYER_POINTER_OSL:
             LED_OFF(LED_1);
             LED_ON(LED_2);
@@ -392,7 +396,7 @@ report_mouse_t pointing_device_task_user(report_mouse_t mouse_report) {
 #        ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
     if (abs(mouse_report.x) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD || abs(mouse_report.y) > CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_THRESHOLD) {
         if (auto_pointer_layer_timer == 0) {
-            layer_on(LAYER_POINTER_OSL);
+            layer_on(LAYER_POINTER);
 #            ifdef RGB_MATRIX_ENABLE
             rgb_matrix_mode_noeeprom(RGB_MATRIX_NONE);
             rgb_matrix_sethsv_noeeprom(HSV_GREEN);
@@ -430,7 +434,7 @@ void matrix_scan_user(void) {
 #    ifdef CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_ENABLE
     if (auto_pointer_layer_timer != 0 && TIMER_DIFF_16(timer_read(), auto_pointer_layer_timer) >= CHARYBDIS_AUTO_POINTER_LAYER_TRIGGER_TIMEOUT_MS) {
         auto_pointer_layer_timer = 0;
-        layer_off(LAYER_POINTER_OSL);
+        layer_off(LAYER_POINTER);
 #        ifdef RGB_MATRIX_ENABLE
         rgb_matrix_mode_noeeprom(RGB_MATRIX_STARTUP_MODE);
 #        endif // !RGB_MATRIX_ENABLE
