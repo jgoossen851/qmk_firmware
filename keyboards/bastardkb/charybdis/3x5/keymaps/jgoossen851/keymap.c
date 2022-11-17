5581bf5b2db44fa90bf5014c3ff03dfdba672be4
Jeremy Goossen <jeremyg995@gmail.com>
2022-11-17 14:44:36 -0600

    Move Zero and Dot to left thumb to keep all numbers on the same hand at the expense of entirely consistent layer navigation; keep old positions too until tested

--- keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
+++ keyboards/bastardkb/charybdis/3x5/keymaps/jgoossen851/keymap.c
@@ -135,7 +135,7 @@ static uint16_t auto_pointer_layer_timer = 0;
      KC_ESC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,   KC_DEL, KC_SLSH, KC_QUES, XXXXXXX, KC_BSPC, \
      KC_TAB,  KC_DLR, KC_PERC, KC_CIRC, KC_MINS,  CAPSWRD, ___HOME_ROW_MODS_ACS_R___,  KC_ENT, \
     KC_LBRC, KC_EXLM,   KC_AT, KC_HASH, KC_RBRC,  KC_CAPS, TO_SYMB, TO_PNTR, OSL_FUN, KC_RGUI, \
-                      _______, TO_ALPH, _______,  TO_NUMB, TO_SYMB
+                      OSL_PNT, TO_ALPH, OSL_NAV,  TO_NUMB, TO_SYMB
 
 
 /**
@@ -149,7 +149,7 @@ static uint16_t auto_pointer_layer_timer = 0;
      KC_ESC,    KC_7,    KC_8,    KC_9,  KC_EQL,   KC_DEL, KC_SLSH,    KC_0, KC_PDOT, KC_BSPC, \
      KC_TAB,    KC_4,    KC_5,    KC_6, KC_MINS,  KC_COLN, OSM_SFT, KC_RCTL, KC_LALT,  KC_ENT, \
      KC_GRV,    KC_1,    KC_2,    KC_3, KC_BSLS,  XXXXXXX, TO_NUMB, TO_NAVG, TO_HXNP, KC_RGUI, \
-                      _______, TO_ALPH, _______,  TO_NUMB, OSL_SYM
+                      OSL_PNT,    KC_0, KC_PDOT,  TO_NUMB, OSL_SYM
 
 
 /**
@@ -192,7 +192,7 @@ static uint16_t auto_pointer_layer_timer = 0;
      KC_F12,   KC_F7,   KC_F8,   KC_F9, KC_PSCR,  XXXXXXX, XXXXXXX,  KC_APP, XXXXXXX, XXXXXXX, \
      KC_F11,   KC_F4,   KC_F5,   KC_F6, KC_SCRL,  XXXXXXX, ___HOME_ROW_MODS_ACS_R___, XXXXXXX, \
      KC_F10,   KC_F1,   KC_F2,   KC_F3, KC_PAUS,  XXXXXXX, TO_FUNC, TO_TOOL, TO_NUMB, KC_RGUI, \
-                      _______, TO_ALPH, _______,  TO_NUMB, TO_SYMB
+                      OSL_PNT, TO_ALPH, OSL_NAV,  TO_NUMB, TO_SYMB
 
 
 /**
@@ -219,7 +219,7 @@ static uint16_t auto_pointer_layer_timer = 0;
        KC_C, KC_KP_7, KC_KP_8, KC_KP_9,    KC_F,   KC_DEL, KC_SLSH, KC_KP_0, KC_PDOT, KC_BSPC, \
        KC_B, KC_KP_4, KC_KP_5, KC_KP_6,    KC_E,  KC_COLN, KC_ASTR, KC_MINS, KC_PLUS, KC_PENT, \
        KC_A, KC_KP_1, KC_KP_2, KC_KP_3,    KC_D,  KC_CAPS, TO_HXNP, TO_MEDA, OSL_SYM,  KC_NUM, \
-                      _______, TO_ALPH, _______,  OSL_NUM, OSL_SYM
+                      OSL_PNT, KC_KP_0, KC_PDOT,  OSL_NUM, OSL_SYM
 
 /**
  * \brief Tools layer.
