1424b4bc079f419a0c2aef574fe6ae3e2843b057
Jeremy Goossen <jeremyg995@gmail.com>
2024-01-07 21:28:10 -0600

    Make the Helium-71 default keymap basically useable

--- keyboards/handwired/helium/71/keymaps/default/keymap.c
+++ keyboards/handwired/helium/71/keymaps/default/keymap.c
@@ -20,57 +20,73 @@
 enum helium_keymap_layers {
     LAYER_BASE = 0,
     LAYER_LOWER,
-    LAYER_RAISE,
+    LAYER_RIGHT,
+    LAYER_LEFT,
 };
 
 #define LOWER MO(LAYER_LOWER)
-#define RAISE MO(LAYER_RAISE)
-
-#define CTL_BSP CTL_T(KC_BSPC)
-#define SFT_SPC SFT_T(KC_SPC)
-#define GUI_ENT GUI_T(KC_ENT)
+#define LEFT  MO(LAYER_LEFT)
+#define RIGHT MO(LAYER_RIGHT)
 
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+// BASE LAYER (the 5 leftmost keys are optional and can be set in custom layout)
   [LAYER_BASE] = LAYOUT(
   // ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮    ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮
-       KC_INS,  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,         KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL, KC_BSLS,  KC_GRV,
+      XXXXXXX,  KC_ESC,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,         KC_7,    KC_8,    KC_9,    KC_0, KC_MINS,  KC_EQL,  KC_GRV, KC_BSPC,
   // ├────────┼────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┼────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴────────┤
-       KC_DEL,      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,      KC_BSPC,
+      XXXXXXX,      KC_TAB,    KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,         KC_Y,    KC_U,    KC_I,    KC_O,    KC_P, KC_LBRC, KC_RBRC,      KC_BSLS,
   // ├────────┼────────────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──╮ ╰──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴─────────────┤
-      KC_HOME,        KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,             KC_ENT,
+      XXXXXXX,        KC_CAPS,    KC_A,    KC_S,    KC_D,    KC_F,    KC_G,         KC_H,    KC_J,    KC_K,    KC_L, KC_SCLN, KC_QUOT,             KC_ENT,
   // ├────────┼────────┬──────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───╮╰───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴──────────┬────────┤
-       KC_END,   LOWER,   KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,        KC_RSFT,   RAISE,
+      XXXXXXX,   RIGHT,   KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,         KC_N,    KC_M, KC_COMM,  KC_DOT, KC_SLSH,        KC_RSFT,    LEFT,
   // ├────────┼────────┴─┬────────┴─┬──────┴───┬────┴─────┬──┴────────┴────────┤    ├────────┴────────┴───────┬┴────────┴┬───────┴──┬──────────┬─┴────────┤
-        LOWER,   KC_RCTL,   KC_LGUI,   KC_LALT,     LOWER,              KC_SPC,                        KC_SPC,   KC_RALT,   KC_LGUI,    KC_APP,   KC_RCTL
+      XXXXXXX,   KC_RCTL,   KC_LGUI,   KC_LALT,     LOWER,              KC_SPC,                        KC_SPC,   KC_RALT,   KC_LGUI,    KC_APP,   KC_RCTL
   // ╰────────┴──────────┴──────────┴──────────┴──────────┴────────────────────╯    ╰─────────────────────────┴──────────┴──────────┴──────────┴──────────╯
   ),
 
+  // NAVIGATION LAYER (based on vim HJKL navigation with inverted-T option)
   [LAYER_LOWER] = LAYOUT(
   // ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮    ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮
-      RGB_TOG, KC_MNXT, KC_MPLY,    KC_R,    KC_R,    KC_R, KC_MPRV, XXXXXXX,      KC_LBRC,    KC_R,    KC_R,    KC_R,    KC_7,    KC_8,    KC_9, KC_RBRC,
+      XXXXXXX,  KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,  EE_CLR, QK_BOOT,      XXXXXXX, KC_PSCR, KC_SCRL, KC_PAUS, XXXXXXX, XXXXXXX, XXXXXXX, KC_BSPC,
   // ├────────┼────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┼────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴────────┤
-      KC_LGUI,     KC_LALT,    KC_F,    KC_F,    KC_F, KC_LSFT, XXXXXXX,      KC_PPLS,    KC_F,    KC_F,    KC_F,    KC_4,    KC_5,    KC_6,      KC_PMNS,
+      XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
   // ├────────┼────────────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──╮ ╰──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴─────────────┤
-      XXXXXXX,           KC_V,    KC_V,    KC_V,  EE_CLR, QK_BOOT, KC_PAST,         KC_V,    KC_V,    KC_V,    KC_1,    KC_2,    KC_3,            KC_PSLS,
+      XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT, XXXXXXX,  KC_INS,             KC_ENT,
   // ├────────┼────────┬──────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───╮╰───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴──────────┬────────┤
-         KC_Z, _______,      KC_C,    KC_V,    KC_V,    KC_V,    KC_V,    KC_B,         KC_N,    KC_V,    KC_V,    KC_V,    KC_M,          KC_UP, XXXXXXX,
+      XXXXXXX, XXXXXXX,   KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_HOME, KC_PGDN, KC_PGUP,  KC_END,  KC_DEL,          KC_UP, XXXXXXX,
   // ├────────┼────────┴─┬────────┴─┬──────┴───┬────┴─────┬──┴────────┴────────┤    ├────────┴────────┴───────┬┴────────┴┬───────┴──┬──────────┬─┴────────┤
-      _______,   SFT_SPC,   SFT_SPC,   SFT_SPC,   _______,             SFT_SPC,                       SFT_SPC,   SFT_SPC,   KC_LEFT,   KC_DOWN,   KC_RGHT
+      XXXXXXX,   KC_RCTL,   KC_LGUI,   KC_LALT,   _______,             XXXXXXX,                       XXXXXXX,   XXXXXXX,   KC_LEFT,   KC_DOWN,   KC_RGHT
   // ╰────────┴──────────┴──────────┴──────────┴──────────┴────────────────────╯    ╰─────────────────────────┴──────────┴──────────┴──────────┴──────────╯
   ),
 
-  [LAYER_RAISE] = LAYOUT(
+  // NUMBER PAD
+  [LAYER_LEFT] = LAYOUT(
   // ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮    ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮
-      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    KC_R,    KC_R,    KC_R, XXXXXXX,      XXXXXXX,    KC_R,    KC_R,    KC_R, KC_VOLU, KC_MUTE, KC_VOLD, XXXXXXX,
+      XXXXXXX,  KC_ESC, XXXXXXX, XXXXXXX, XXXXXXX, KC_PSLS, KC_PAST, KC_PMNS,      QK_BOOT,  EE_CLR, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
   // ├────────┼────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┼────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴────────┤
-      KC_LEFT,       KC_UP, KC_DOWN, KC_RGHT,    KC_F,    KC_F,    KC_F,      XXXXXXX,    KC_F,    KC_F,    KC_F, KC_RSFT, KC_RCTL, KC_RALT,      KC_RGUI,
+      XXXXXXX,      KC_TAB, XXXXXXX,   KC_P7,   KC_P8,   KC_P9, KC_PPLS,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
   // ├────────┼────────────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──╮ ╰──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴─────────────┤
-      KC_HOME,        KC_PGUP, KC_PGDN,  KC_END,    KC_V,    KC_V,    KC_V,      QK_BOOT,    KC_V,    KC_V,    KC_V,  EE_CLR, XXXXXXX,            XXXXXXX,
+      XXXXXXX,        KC_PENT, XXXXXXX,   KC_P4,   KC_P5,   KC_P6, KC_PEQL,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,            XXXXXXX,
   // ├────────┼────────┬──────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───╮╰───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴──────────┬────────┤
-         KC_Z,    KC_X,      KC_C,    KC_V,    KC_V,    KC_V,    KC_V,    KC_B,         KC_N,    KC_V,    KC_V,    KC_M, KC_COMM,         KC_DOT, _______,
+      XXXXXXX, XXXXXXX,   KC_LSFT, XXXXXXX,   KC_P1,   KC_P2,   KC_P3, XXXXXXX,      XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,        KC_RSFT, _______,
   // ├────────┼────────┴─┬────────┴─┬──────┴───┬────┴─────┬──┴────────┴────────┤    ├────────┴────────┴───────┬┴────────┴┬───────┴──┬──────────┬─┴────────┤
-      _______,   _______,   SFT_SPC,   SFT_SPC,   SFT_SPC,             SFT_SPC,                       SFT_SPC,   _______,   SFT_SPC,   SFT_SPC,   SFT_SPC
+      XXXXXXX,   KC_RCTL,   KC_LGUI,   KC_LALT,   KC_PDOT,               KC_P0,                       XXXXXXX,   KC_RALT,   KC_LGUI,    KC_APP,   KC_RCTL
+  // ╰────────┴──────────┴──────────┴──────────┴──────────┴────────────────────╯    ╰─────────────────────────┴──────────┴──────────┴──────────┴──────────╯
+  ),
+
+  // FUNCTIONS
+  [LAYER_RIGHT] = LAYOUT(
+  // ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮    ╭────────┬────────┬────────┬────────┬────────┬────────┬────────┬────────╮
+      XXXXXXX,  KC_ESC,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,        KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,  KC_F12, XXXXXXX, KC_BSPC,
+  // ├────────┼────────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┼────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴────────┤
+      XXXXXXX,     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX, KC_BRID, KC_BRIU, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      XXXXXXX,
+  // ├────────┼────────────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──╮ ╰──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴──┬─────┴─────────────┤
+      XXXXXXX,        XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,      KC_MPRV, KC_VOLD, KC_VOLU, KC_MNXT, XXXXXXX, XXXXXXX,             KC_ENT,
+  // ├────────┼────────┬──────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───╮╰───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴───┬────┴──────────┬────────┤
+      XXXXXXX, _______,   KC_LSFT, XXXXXXX,  KC_CUT, KC_COPY, KC_PSTE, XXXXXXX,      XXXXXXX, KC_MPLY, KC_MUTE, XXXXXXX, XXXXXXX,        KC_RSFT, XXXXXXX,
+  // ├────────┼────────┴─┬────────┴─┬──────┴───┬────┴─────┬──┴────────┴────────┤    ├────────┴────────┴───────┬┴────────┴┬───────┴──┬──────────┬─┴────────┤
+      XXXXXXX,   KC_RCTL,   KC_LGUI,   KC_LALT,   XXXXXXX,             XXXXXXX,                       XXXXXXX,   KC_RALT,   KC_LGUI,    KC_APP,   KC_RCTL
   // ╰────────┴──────────┴──────────┴──────────┴──────────┴────────────────────╯    ╰─────────────────────────┴──────────┴──────────┴──────────┴──────────╯
   ),
 };
---
+++ keyboards/handwired/helium/71/rules.mk
@@ -0,0 +1,4 @@
+# Build Options
+
+MOUSEKEY_ENABLE = no       # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
